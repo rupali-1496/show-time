bfecac0fb88a2ac52033008d1784fe77c39e8341
rupali ranjan <rupali.ranjan1496@gmail.com>
2020-02-19 13:31:59 +0530

    ShowTime Version 1(added AppConstants.java file)

---
+++ app/src/main/java/com/example/showtime/utils/AppConstants.java
@@ -0,0 +1,18 @@
+package com.example.showtime.utils;
+
+public class AppConstants {
+
+    public static final String SHARED_PREFERENCE = "LoginDetails";
+
+    public static final String SHARED_PREF_KEY = "login";
+
+    public static final String EMAIL_ID_1 = "rupali.ranjan1496@gmail.com";
+
+    public static final String EMAIL_ID_2 = "rupali.ranjan1496@gmail.com";
+
+    public static final String PSSWORD = "rupali@14";
+
+
+
+
+}
